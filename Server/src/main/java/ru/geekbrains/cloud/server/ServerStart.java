55c97e2d6feeab8c39e1f1236506d387234e1bd6
IMBABOT1 <zlovred12@gmail.com>
2021-02-17 20:32:42 +0300

    dsafsdaf

--- Server/src/main/java/ru/geekbrains/cloud/server/ServerStart.java
+++ Server/src/main/java/ru/geekbrains/cloud/server/ServerStart.java
@@ -1,11 +1,17 @@
 package ru.geekbrains.cloud.server;
 
-public class ServerStart {
-//    ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-//    Server server = context.getBean("server", Server.class);
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-//    ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-//    Server server = context.getBean("server", Server.class);
+public class ServerStart {
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+    Server server = context.getBean("server", Server.class);
+
+
+    ApplicationContext context1 = new AnnotationConfigApplicationContext(SpringConfig.class);
+    Server server1= context.getBean("server", Server.class);
+
 
 
 
